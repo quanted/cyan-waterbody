fe029e63836c99f3190911dd8ee6ee5db552521a
Deron Smith <deron.brock@gmail.com>
2023-03-01 08:59:42 -0500

    changed get report details url

--- wb_flask.py
+++ wb_flask.py
@@ -594,7 +594,7 @@ def get_report_tribes():
     return {"tribes": tribes}, 200
 
 
-@app.route('/waterbody/report/state/')
+@app.route('/waterbody/report/details/')
 def get_waterbody_reports_data():
     args = request.args
     state = None
