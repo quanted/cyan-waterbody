b8dc4910d6f21e60ce4a8b5da854df599623794e
popenc <i.nickpope@gmail.com>
2023-03-10 09:12:28 -0500

    removed CORS from wb_flask

--- wb_flask.py
+++ wb_flask.py
@@ -38,7 +38,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("cyan-waterbody")
 logger.info("CyAN Waterbody Flask App")
 
-cors = CORS(app, expose_headers=["Content-Disposition"])
+# cors = CORS(app, expose_headers=["Content-Disposition"])
 
 celery_handler = CeleryHandler()
 
